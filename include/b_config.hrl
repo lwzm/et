7f417cfb87cd532df56fec575d0b3bdc4c969b1e
felloak <felloak@gmail.com>
2014-02-07 12:06:58 +0800

    improve erlang-template

--- include/b_config.hrl
+++ include/b_config.hrl
@@ -1,5 +1,5 @@
 {# vim:se syntax=html.tornadotmpl: #}
 {% autoescape None %}
 
-{% set ignore = {"id", "name", "icon", "asset"} %}
+{% set ignore = {"id", "name", "icon", "asset", "grow"} %}
 -record(hero_base, { {{", ".join(k for k in sorted(root.heroes[0]) if k not in ignore) }} }).
--- src/b_config.erl
+++ src/b_config.erl
@@ -1,26 +1,33 @@
 {# vim:se syntax=html.tornadotmpl: #}
 {% autoescape None %}
 -module(b_config).
--compile(export_all).
+-export([get/1, set/2]).
+
+set(K, V) -> todo.
 
 {% for x in root.hero_strengthen %}
-hero_strengthen({{ x.main_star }}, {{ x.sub_star }}) -> { {{ x.rate }}, {{ x.point }} };
+get({ hero_strengthen, {{ x.main_star }}, {{ x.sub_star }} }) -> { {{ x.rate }}, {{ x.point }} };
 {% end %}
-hero_strengthen('end', 'end') -> 'end'.
 
 {% set key_head = "hero_strengthen_gold_cost_" %}
 {% for k in sorted(root) %}
 {% if k.startswith(key_head) %}
 {% set i = int(k[len(key_head):]) %}
 {% for x in root[k] %}
-hero_strengthen_gold_cost({{ i }}, {{ x.level }}) -> {{ x.gold_cost }};
+get({ hero_strengthen_gold_cost, {{ i }}, {{ x.level }} }) -> {{ x.gold_cost }};
 {% end %}
 {% end %}
 {% end %}
-hero_strengthen_gold_cost('end', 'end') -> 'end'.
 
-{% set ignore = {"id", "name", "icon", "asset"} %}
+{% set ignore = {"id", "name", "icon", "asset", "grow"} %}
 {% for x in root.heroes %}
-heroes({{ x.id }}) -> { hero_base, {{", ". join(repr(v) for k, v in sorted(x.items()) if k not in ignore)}} };
+get({ heroes, {{ x.id }} }) -> { hero_base, {{", ". join(repr(v) for k, v in sorted(x.items()) if k not in ignore)}} };
 {% end %}
-heroes('end') -> 'end'.
+
+get({ lv_exp, 1 }) -> 100;
+get({ lv_exp, 2 }) -> 200;
+get({ lv_exp, 3 }) -> 300;
+get({ lv_exp, 4 }) -> 400;
+get({ lv_exp, 5 }) -> 500;
+
+get(_) -> "Not Found".
